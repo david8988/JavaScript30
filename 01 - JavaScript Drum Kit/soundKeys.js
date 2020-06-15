f5176609f02b0a37e4f4bd4607141458815262f6
david <63117587+david8988@users.noreply.github.com>
2020-06-15 11:07:59 -0500

    modified program sound keys

---
+++ 01 - JavaScript Drum Kit/soundKeys.js
@@ -0,0 +1,25 @@
+
+
+document.addEventListener('keydown',playSound);
+
+function playSound(e){
+    const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+    const key=document.querySelector(`div[data-key="${e.keyCode}"]`);
+    //console.log(audio)
+    //console.log(e.keyCode)
+    //console.log(key)
+    if(!audio) return;
+    key.classList.add('playing');
+    audio.currentTime=0;
+    audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
